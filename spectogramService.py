27776f80dae58d9e4c27f5e1dd0dbf4d8efba5eb
domeczek <666dobre666@gmail.com>
2019-12-13 22:30:49 +0100

    UPDATE spectogramService

--- spectogramService.py
+++ spectogramService.py
@@ -6,6 +6,7 @@ import numpy as np
 from scipy.io import wavfile
 from scipy import signal
 from skimage import util
+import gc
 import glob
 datatest = (glob.glob("./ptaki/train/wav/*.wav")) #czytanie plików z lokalizacji
 # datatest #pokazuje liste plików
@@ -15,16 +16,16 @@ audio = [None]*len(datatest)
 freqs = [None]*len(datatest)
 times = [None]*len(datatest)
 Sx    = [None]*len(datatest)
-f     = [None]*len(datatest)
-ax    = [None]*len(datatest)
 
-for i in range(len(datatest)):
+for i in range(1, len(datatest)):
     rate[i], audio[i] = wavfile.read(datatest[i])
     freqs[i], times[i], Sx[i] = signal.spectrogram(audio[i], fs=rate[i], window='hanning',
                                       nperseg=1024, noverlap=M - 100,
                                       detrend=False, scaling='spectrum')
-    f[i], ax[i] = plt.subplots(figsize=(4.8, 2.4))
-    ax[i].pcolormesh(times[i], freqs[i] / 1000, 10 * np.log10(Sx[i]), cmap='viridis')
-    ax[i].axis('off')
-    f[i].savefig('./imageptaki/train/'+datatest[i][18:-4]+'.png', bbox_inches='tight', pad_inches=0)
-    plt.close()
+    f, ax = plt.subplots(figsize=(4.8, 2.4))
+    ax.pcolormesh(times[i], freqs[i] / 1000, 10 * np.log10(Sx[i]), cmap='viridis')
+    ax.axis('off')
+    f.savefig('./imageptaki/train/'+datatest[i][18:-4]+'.png', bbox_inches='tight', pad_inches=0)
+    plt.close('all')
+    plt.clf()
+    gc.collect()
